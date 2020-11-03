08644d4e320351db9606e12dcc09fed7fe617225
iyaalfiryal <iyaalfiryal@gmail.com>
2020-11-03 07:11:12 +0700

    Initial commit

---
+++ app/src/main/java/com/iyal/idn/e_commerse/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.iyal.idn.e_commerse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
